b50648f2392da94e7f02517218af77a82dc723df
chenjyp <chenjyp@yonyou.com>
2020-03-17 16:37:52 +0800

    删除登陆后的查询

--- src/App.vue
+++ src/App.vue
@@ -21,14 +21,11 @@ export default {
     // 校验登录信息
     let logintest = {
       "resultType":"Y",
-      "cryptType":"2", 
+      "cryptType":"2",
       "body":"u9su4Mi92g8b2l1GjBEllZQBpjFxRbKYY4MNjgS7qyS4GajzsQbyHPC6Xon2lc+gPjF3kjQalI2WNWw3UpY6M3RU4B6GFEC6IWf6KxYtcF4D9BJ6SrnW+LG1ncaArA8yJaAnlF42uMP4pHnWfJcIwBlobdjBJtCM/jgyvqSDq8EbGuUbEzfTBdWNGul6Zfr4MfvQFNE/KcsETGb6HrXayCR+ZD7aQ6dfk4wdd47Lpw14apcdMK8S3YaoOWN5NTgFgvqN8CyoZWJWFtZvntMozOWfKcgSIC5iTnVQsDYtnyvoPXvLMQRK2RngCMfHNyj8KHmtqLvapwCimVrD34/vV3n4WmQ4d+yPpMthMwg9NH6dxuC92tmfmeSNnfB3Ru3sZyo5PlxCrAyEaP7V2nnMg+JC9KuBf43TcrvQFAMJ9/YzjnYQLw4uVH58q0ebCcCY5Wvoda74aMRM+uWYY8RTFclmCyyyCEcttY1XzDS+zOCDu6rhjiDswDXno+YWbgU3G2skHRoMMxjOGmCUS1u6rrHp+6ERZa/YiRH77PFcilAsIbINkmBKgm8r/qe3m81nO8u2U6pEHp4="
       }
     let loginInfo = this.validateLogin(logintest);
     console.log(loginInfo);
-    this.$Http.getNtcInfo({
-"ntcId": 12, "areaCode":12
-});
     // 柜面不存在登录信息
     if (loginInfo.errMsg) {
       this.login();
